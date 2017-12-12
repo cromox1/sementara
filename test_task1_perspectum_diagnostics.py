3ff46cf3108abc1a616a3f0c76cf89d1dcc270c7
Rosli Talib <xixa01@yahoo.co.uk>
2017-12-12 21:26:57 +0000

    Update test_task1_perspectum_diagnostics.py

--- test_task1_perspectum_diagnostics.py
+++ test_task1_perspectum_diagnostics.py
@@ -2,31 +2,31 @@ input1 = [['g', 'gh', 'ghj', 'g'], ['j', 'ju', 'gh', 'gk', 'gn']]
 # input1 = [['g', 'gh', 'ghj', 'g'], ['j', 'ju', 'gh', 'gk', 'gn'], ['j', 'ju', 'gh']]
 
 ### total number of strings & number of unique string
-def all_unique_strings(file1):
-    jj = len(file1)
+def all_unique_strings(inputlist):
+    jj = len(inputlist)
     inputall=[]
     for j in range(jj):
-        inputall = inputall + file1[j]
+        inputall = inputall + inputlist[j]
     return len(inputall), len(list(set(inputall))), inputall
 
 ### strings appearing in multi lists
-def strings_multilist(file1):
-    jj = len(file1)
+def strings_multilist(inputlist):
+    jj = len(inputlist)
     dict1 = {} ; count2 = {} ; multi = []
     # change lists to dictionary+lists
     for p in range(jj):
         count={}
-        for word in file1[p]:
+        for word in inputlist[p]:
             if word not in count:
                 count[word] = 1
             else:
                 count[word] +=1
         dict1[p] = count
     # count string by list
-    for word in list(set(all_unique_strings(file1)[2])):
+    for word in list(set(all_unique_strings(inputlist)[2])):
         count2[word] = 0
     for p in range(jj):
-        for word in list(set(all_unique_strings(file1)[2])):
+        for word in list(set(all_unique_strings(inputlist)[2])):
             if word in dict1[p].keys():
                 count2[word] += 1
     for ky,vl in count2.items():
@@ -34,12 +34,12 @@ def strings_multilist(file1):
             multi = multi + [ky]
     return multi
 
-multilist = strings_multilist(input1)
-uniquestr = all_unique_strings(input1)[1]
-totalstr = all_unique_strings(input1)[0]
+def testing(inputlist):
+    multilist = strings_multilist(inputlist)
+    uniquestr = all_unique_strings(inputlist)[1]
+    totalstr = all_unique_strings(inputlist)[0]
+    print('Strings appearing in multiple lists : ',end=''); print(*multilist, sep=', ')
+    print('Number of unique strings : ' + str(uniquestr))
+    print('Total number of strings processed : ' + str(totalstr))
 
-print('Strings appearing in multiple lists : ',end=''); print(*multilist, sep=', ')
-print('Number of unique strings : ' + str(uniquestr))
-print('Total number of strings processed : ' + str(totalstr))
-
-
+testing(input1)
