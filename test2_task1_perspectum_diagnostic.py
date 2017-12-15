089629603cd4c1ea6a81d4ba39b7025561ad891e
Rosli Talib <xixa01@yahoo.co.uk>
2017-12-15 19:11:47 +0000

    Create test2_task1_perspectum_diagnostic.py

---
+++ test2_task1_perspectum_diagnostic.py
@@ -0,0 +1,45 @@
+input1 = [['g', 'gh', 'ghj', 'g'], ['j', 'ju', 'gh', 'gk', 'gn']]
+# input1 = [['g', 'gh', 'ghj', 'g'], ['j', 'ju', 'gh', 'gk', 'gn'], ['j', 'ju', 'gh'], ['x','gk']]
+# input1 = [['g', 'gh', 'ghj', 'g']]
+# input1 = [['g', 'gh', 'ghj', 'g', 'ghj'],['g']]
+# input1 = ['g', 'gh', 'ghj', 'g']
+# input1 = {'g', 'gh', 'ghj', 'g'}
+
+import pandas as pd
+from itertools import zip_longest
+
+def convert2df(inputlist):
+    allcol = len(inputlist)
+    inputtmp = list(map(list, zip_longest(*inputlist))) ## transform input/dataframe format - Transpose
+    inputdf = pd.DataFrame(inputtmp, columns=['col'+str(x) for x in range(allcol)]) ## convert to DataFrame
+
+    uniqnone = pd.unique(inputdf[list(inputdf)].values.ravel()).tolist()
+    uniq = [ xx for xx in uniqnone if xx is not None ]
+    outputlist = pd.DataFrame(uniq, columns=['uniq'])
+
+    ### compare uniq matrix with lists (in DataFrame = col<number> )
+    for x in range(allcol):
+        colname = 'col'+str(x)
+        outputlist = outputlist.join(pd.DataFrame(uniq, columns=[colname]).isin(list(inputdf[colname])))
+
+    ## create sum of 'True' value's column
+    outputlist = outputlist.join(pd.DataFrame(list(outputlist.sum(axis=1)), columns=['TRUE']))
+
+    return outputlist, inputdf, uniq
+
+def testing(inputlist):
+    if type(inputlist) == list:
+        print('') ; print('input = '+str(inputlist))
+        if type(inputlist[0]) != list:
+            inputlist = [inputlist]
+        print('') ; print(convert2df(inputlist)[1].fillna('')) ; print('')
+        print('uniq = '+ str(convert2df(inputlist)[2]))
+        print('') ; print(convert2df(inputlist)[0]) ; print('')
+        multilist = list(convert2df(inputlist)[0]['uniq'].loc[convert2df(inputlist)[0]['TRUE']>1 ])
+        print('Strings appearing in multiple lists : ', end=''); print(*multilist, sep=', ')
+        print('Number of unique strings : '+str(len(convert2df(inputlist)[0]['uniq'].axes[0])))
+        print('Total number of strings processed : '+str(int(convert2df(inputlist)[1].apply(pd.value_counts).fillna(0).sum().sum())))
+    else:
+        print('Error - input is not a list')
+
+testing(input1)
